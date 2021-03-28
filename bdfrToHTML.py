128350e0ec1fb5206dc2af5860065798fd039d71
BlipRanger <blipranger@ghost.tel>
2021-03-28 16:40:44 -0400

    Many changes, first decent functional version.

--- bdfrToHTML.py
+++ bdfrToHTML.py
@@ -3,11 +3,11 @@ import os
 import markdown
 from datetime import datetime
 import click
+import shutil
 
 
-workingDir = './posts'
-rootDir = os.getcwd()
-outputDir = rootDir + '/html'
+inputFolder = ''
+outputFolder = ''
 
 def jsonToHTML(file_path):
     f = open(file_path,)
@@ -17,19 +17,18 @@ def jsonToHTML(file_path):
     return data
 
 
-def findMatchingMedia(name):
+def findMatchingMedia(name, folder):
     paths = []
-    for dirpath, dnames, fnames in os.walk(workingDir):
+    for dirpath, dnames, fnames in os.walk(folder):
         for f in fnames:
             if (name) in f and not f.endswith('.json'):
-                paths.append(os.path.join(dirpath, f))
+                paths.append(copyMedia(os.path.join(dirpath, f), f))
     return paths
 
 
 def buildGallery(paths):
     html = '<div class=photoGrid><div class=row>'
     for p in paths:
-        p = '.' + p
         html = html + """<div class=column><a href={path}><img src={path} style="width:100%"></a></div>""".format(path=p)
     html = html + """</div></div>"""
     return html
@@ -41,14 +40,12 @@ def formatMatchingMedia(paths):
         path = paths[0]
 
         if path.endswith('jpg') or path.endswith('jpeg') or path.endswith('png'):
-            path = '.' + path
             return '<a href={path}><img src={path}></a>'.format(path=path)
         elif path.endswith('m4a') or path.endswith('mp4') or path.endswith('mkv'):
-            path = '.' + path
             return '<video width="320" height="240" controls><source src="{path}"></video>'.format(path=path)
         elif path.endswith('txt'):
             txt = '<div>'
-            with open(path, 'r') as file:
+            with open(outputFolder + path, 'r') as file:
                 lines = file.readlines()
             file.close()
             for l in lines[1:]:
@@ -62,6 +59,11 @@ def formatMatchingMedia(paths):
         return buildGallery(paths)
     return ""
 
+def copyMedia(mediaPath, filename):
+    writeFolder = outputFolder + 'media/' 
+    assure_path_exists(writeFolder)
+    shutil.copyfile(mediaPath, writeFolder + filename)
+    return 'media/' + filename
 
 def writeReplies(comment, data):
     html = ''
@@ -95,21 +97,36 @@ def writeHead():
             </head><body>"""
 
 def writePost(data):
+    matchingMedia = findMatchingMedia(data['id'], inputFolder)
+    content = formatMatchingMedia(matchingMedia)
+
+    if len(matchingMedia) == 0:
+        local_link = ''
+    else:
+        local_link = matchingMedia[0]
+
     return """
         <div class=post>
         <h1>{title}</h1>
         <div class="info">
         <div class="links">
-        <a href=".{local_link}">content link</a> <a href="{link}">reddit link</a> | <a href="https://www.reddit.com{url}"></a></div>
+        <a href="{local_link}">link</a> <a href="{link}">reddit link</a> <a href="https://www.reddit.com{url}"> Content Link</a> </div>
         </div>
+        <time>{time}</time><span class="subreddit">{subreddit}</span><span class="user">{user}</span>
         <div class=content>{content}</p></div>
         </div>
 
-        """.format(local_link=findMatchingMedia(data['id'])[0], submission=data.get('id', ''), content=formatMatchingMedia(findMatchingMedia(data['id'])), url=data.get('permalink', ''), link=data.get('url',''), title=data.get('title',''))
+        """.format(time=writeDatestring(data.get('time', '1616957979')), local_link=local_link, submission=data.get('id', ''),
+         content=content, url=data.get('permalink', ''), link=data.get('url',''), title=data.get('title',''), user=data.get('author', ''),
+         subreddit=getSubreddit(data.get('permalink', '')))
+
+def getSubreddit(permalink):
+    strings = permalink.split("/")
+    return strings[1] + '/' + strings[2]
 
 def writeToHTML(data):
-    file_path = './html/' + data['id'] + '.html'
-    with open(file_path, 'w') as file:
+    file_path = data['id'] + '.html'
+    with open(outputFolder + file_path, 'w') as file:
         html = writeHead()
         html = html + writePost(data) + "<h2>Comments</h2><div class=comments>"
         for c in data['comments']:
@@ -120,18 +137,34 @@ def writeToHTML(data):
         file.write(html)
     return file_path
 
+def assure_path_exists(path):
+        dir = os.path.dirname(path)
+        if not os.path.exists(dir):
+                os.makedirs(dir)
 
+@click.command()
+@click.option('--input', default='.', help='The folder where the download and archive results have been saved to')
+@click.option('--output', default='./html/', help='Folder where the HTML results should be created.')
+def converter(input, output):
+    global inputFolder
+    global outputFolder
+    inputFolder = input
+    outputFolder = output
+    html = writeHead()
 
-html = writeHead()
+    for dirpath, dnames, fnames in os.walk(input):
+        for f in fnames:
+            if f.endswith(".json"):
+                data = jsonToHTML(os.path.join(dirpath, f))
+                html = html + '<a href={local_path}>{post}</a>'.format(post=writePost(data), local_path=data['htmlPath'])
 
-for dirpath, dnames, fnames in os.walk(workingDir):
-    for f in fnames:
-        if f.endswith(".json"):
-            data = jsonToHTML(os.path.join(dirpath, f))
-            html = html + '<a href={local_path}>{post}</a>'.format(post=writePost(data), local_path=data['htmlPath'])
+    file_path = output + '/index.html'
+    assure_path_exists(output)
+    with open(file_path, 'w') as file:
+        html = html + """</body>
+                </html>"""    
+        file.write(html)
+    shutil.copyfile('style.css', outputFolder + 'style.css')
 
-file_path = 'saved.html'
-with open(file_path, 'w') as file:
-    html = html + """</body>
-            </html>"""    
-    file.write(html)
+if __name__ == '__main__':
+    converter()
