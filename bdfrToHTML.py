1f0d70d7cbd0b2de17881070b170ea737cbe12ab
BlipRanger <1860540+BlipRanger@users.noreply.github.com>
2021-05-12 13:40:41 -0400

    Let's try templates with jinja2

--- bdfrToHTML.py
+++ bdfrToHTML.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 __author__ = "BlipRanger"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __license__ = "GNU GPLv3"
 
 import json
@@ -14,20 +14,15 @@ import requests
 import logging
 import subprocess
 import time
+import jinja2
 
 #Logging Setup
-level = os.environ['BDFRH_LOGLEVEL']
+level = os.getenv('BDFRH_LOGLEVEL', "INFO")
 if level == "DEBUG":
     logging.basicConfig(level=logging.DEBUG)
 else:
     logging.basicConfig(level=logging.INFO)
 
-
-#Globals
-inputFolder = ''
-outputFolder = ''
-recoverComments = False
-context = False
 
 #Loads in the json file data and adds the path of it to the dict
 def loadJson(file_path):
@@ -114,16 +109,6 @@ def copyMedia(mediaPath, filename):
     logging.debug('Moved ' + mediaPath + ' to ' + writeFolder +filename)
     return 'media/' + filename
 
-#Handle writing replies to comments
-#Uses the writeTopLevelComment function to unroll comment/reply chains
-def writeReplies(comment, data):
-    html = ''
-    if not comment['replies']:
-        return ''
-    for replies in comment['replies']:
-        html = html + writeTopLevelComment(replies, data)
-    return html
-
 #Convert unix time string to datetime
 def writeDatestring(time):
     time = int(time)
@@ -160,121 +145,10 @@ def archiveContext(link):
     shutil.rmtree(inputFolder + "context/")
     return data['htmlPath']
 
-
-#Generate the html for a comment
-def writeTopLevelComment(comment, data):
-    if not comment:
-        return ''
-    if recoverComments and (comment.get('author', '') == 'DELETED' or comment.get('body', '') == '[removed]'):
-        comment = recoverDeletedComment(comment) 
-    return """<div id={id} class="comment">
-    <div class="info {recovered}">
-    <a href="https://www.reddit.com{commentLink}"><div class="time">{created}</div></a>
-    <div class="score">({score})</div>
-    <div class=author>u/{author}</div></div>
-    <p>{body}</p><div class=reply>{replies}</div></div>""".format(body=markdown.markdown(comment['body']), 
-    created=writeDatestring(comment['created_utc']), score=comment['score'], author=comment['author'],
-     id=comment['id'], replies=writeReplies(comment, data), commentLink=data.get('permalink','') + comment['id'],
-     recovered=comment.get('recovered', ''))
-
-
-#Generate the html for the html head/page start
-def writeHead():
-    return """<html>
-            <head>
-            <style>body {background-color: rgb(35, 35, 35);}</style>
-            <link rel='stylesheet' type='text/css' href='style.css'>
-            <meta charset="utf-8"/>
-            </head><body>"""
-
-#Write the html body of a given post 
-#Input: dict created from the json file
-def writePost(data):
-    matchingMedia = findMatchingMedia(data['id'], inputFolder)
-    content = formatMatchingMedia(matchingMedia)
-
-    if len(matchingMedia) == 0:
-        local_link = ''
-    else:
-        local_link = matchingMedia[0]
-
-    return """
-        <div class=post>
-            <h1>{title}</h1>
-            <div class="info">
-                <div class="links">
-                    <a href="{local_link}"> link</a> <a href="{link}">Content link</a> <a href="https://www.reddit.com{url}"> Reddit Link</a> 
-                </div>
-                <time>{time}</time><a href='https://reddit.com/{subreddit}'><span class="subreddit">{subreddit}</span></a><a href='https://reddit.com/u/{user}'><span class="user">u/{user}</span></a>
-            </div>
-            <div class=content>{content}</div>
-            
-        </div>
-
-        """.format(time=writeDatestring(data.get('created_utc', '1616957979')), local_link=local_link, submission=data.get('id', ''),
-         content=content, url=data.get('permalink', ''), link=data.get('url',''), title=data.get('title',''), user=data.get('author', ''),
-         subreddit=getSubreddit(data.get('permalink', '')))
-
-#Function to write a saved comment in a 'post' format
-def writeCommentPost(comment):
-    if not comment:
-        return ''
-    if recoverComments and (comment.get('author', '') == 'DELETED' or comment.get('body', '') == '[removed]'):
-        comment = recoverDeletedComment(comment) 
-
-    #Generate Permalink
-    permalink = 'https://www.reddit.com/r/{subreddit}/comments/{submission}/{title}/{id}'.format(id=comment['id'],
-     submission=comment['submission'], title=comment['submission_title'], subreddit=comment['subreddit'])
-
-    contextLink = ''
-    if context:
-        contextLink = archiveContext(permalink)
-
-    return """
-        <div class="post">
-            <h1>Comment on {title}</h1>
-            <div class="info {recovered}">
-                <div class="links">
-                    <a href="{url}"> Reddit Link</a><a href={contextLink}>Context Link</a> 
-                </div>
-                <time>{time}</time><a href='https://reddit.com/r/{subreddit}'><span class="subreddit">{subreddit}</span></a><a href='https://reddit.com/u/{user}'><span class="user">u/{user}</span></a>
-            </div>
-            <div class=content>{content}</div>
-            
-        </div>
-
-        """.format(time=writeDatestring(comment.get('created_utc', '1616957979')), submission=comment.get('id', ''),
-         content=markdown.markdown(comment.get('body', '')), url=permalink, link=permalink, title=comment.get('submission_title',''), user=comment.get('author', ''),
-         subreddit=comment.get('subreddit', ''), recovered=comment.get('recovered', ''), contextLink=contextLink)
-             
-
 #Extract the subreddit name from the permalink
 def getSubreddit(permalink):
     strings = permalink.split("/")
     return strings[1] + '/' + strings[2]
-
-#Write html file from given post archive info
-def writeToHTML(data):
-    file_path = data['id'] + '.html'
-    path = os.path.join(outputFolder, file_path)
-    with open(path, 'w') as file:
-        html = writeHead()
-        if data.get('parent_id', None) is None:
-            html = html + writePost(data) + "<h2>Comments</h2><div class=comments>"
-            for c in data['comments']:
-                html = html + writeTopLevelComment(c, data)
-
-                html = html + """</div></body>
-                        </html>"""    
-        else:
-            html = html + writeCommentPost(data) + "<h2>Comments</h2><div class=comments>"
-            for c in data['replies']:
-                html = html + writeTopLevelComment(c, data)
-
-                html = html + """</div></body>
-                        </html>"""
-        file.write(html)
-    return file_path
 
 #Check for path, create if does not exist
 def assure_path_exists(path):
@@ -286,64 +160,33 @@ def assure_path_exists(path):
 #formats/writes archive data and media to html files, creates a single index.html file with 
 #links to all archived posts. 
 def main():
-    
-    #Begin main process
-    datalist = []
-
-    file_path = os.path.join(outputFolder, 'idList.txt')
-    if os.path.isfile(file_path):
-        with open(file_path, 'r') as f:
-            datalist = list(f)
-    
+        
     assure_path_exists(outputFolder)
     if not os.path.isdir(inputFolder):
         raise ValueError('Input folder does not exist') 
-    html = writeHead()
-    postCount = 0
-    pageCount = 1
+
+
+    templateLoader = jinja2.FileSystemLoader(searchpath="./templates")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    templateEnv.filters["markdown"] = markdown.markdown
+    TEMPLATE_FILE = "post.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    
+    postList = []
     for dirpath, dnames, fnames in os.walk(inputFolder):
         for f in fnames:
             if f.endswith(".json"):
                 data = loadJson(os.path.join(dirpath, f))
-                data['htmlPath'] = writeToHTML(data)
-                if postCount == 25:
-                    file_path = os.path.join(outputFolder, 'page{pageCount}.html'.format(pageCount=pageCount))
-                    with open(file_path, 'w') as file:
-                        html = html + """<div class=footer><div class=previousPage><a href='page{previous}.html'>Previous Page</a></div>
-                        <div class=nextPage><a href='page{next}.html'>Next Page</a></div></div>
-                        </body>
-                </html>""".format(previous=pageCount-1, next=pageCount+1)
-                        file.write(html)
-                    html = writeHead()
-                    pageCount = pageCount + 1
-                    postCount = 0
-                if data.get('parent_id', None) is None:
-                    html = html + '<a href={local_path}>{post}</a>'.format(post=writePost(data), local_path=data['htmlPath'])
-                else:
-                    html = html + '<a href={local_path}>{post}</a>'.format(post=writeCommentPost(data), local_path=data['htmlPath'])
-                postCount = postCount + 1
-                datalist.append(data['id'] + "\n")
+                html = template.render(data)
+                filename = os.path.join(outputFolder, data['id']+".html")
+                with open(filename, 'w') as file:
+                    file.write(html)
+                postList.append(data)
 
-    file_path = os.path.join(outputFolder, 'page{pageCount}.html'.format(pageCount=pageCount))
-    with open(file_path, 'w') as file:
-        html = html + """<div class=footer><div class=previousPage><a href='page{previous}.html'>Previous Page</a></div>
-                         <div class=nextPage><a href='page{next}.html'>Next Page</a></div></div>
-                         </body>
-                </html>""".format(previous=pageCount-1, next=pageCount+1)
-        file.write(html)
-    html = writeHead()
-
-
-    file_path = os.path.join(outputFolder, 'index.html')
-    with open(file_path, 'w') as file:
-        html = html + """
-        <meta http-equiv="refresh" content="0; URL='page1.html'" /></div></body>
-                </html>"""    
-        file.write(html)
-
-    file_path = os.path.join(outputFolder, 'idList.txt')
-    with open(file_path, 'w') as file:
-        file.writelines(datalist)
+    TEMPLATE_FILE = "index.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    
+    
 
     shutil.copyfile('style.css', os.path.join(outputFolder, 'style.css'))
     logging.info("Run Complete!")
@@ -354,10 +197,8 @@ def main():
 @click.option('--output', default='./html/', help='Folder where the HTML results should be created.')
 @click.option('--recover_comments', default=False, type=bool, help='Should we attempt to recover deleted comments?')
 @click.option('--archive_context', default=False, type=bool, help='Should we attempt to archive the contextual post for saved comments?')
-@click.option('--watch_folder', default=False, type=bool, help='After the first run, watch the input folder for changes and rerun when detected')
-@click.option('--watch_freq', default=1, help='How often should we recheck the watched input folder in minutes. Requires watch_folder be enabled')
 @click.option('--delete_input', default=False, type=bool, help='Should we delete the input after creating the output?')
-def converter(input, output, recover_comments, archive_context, watch_folder, watch_freq, delete_input):
+def converter(input, output, recover_comments, archive_context, delete_input):
     global inputFolder
     global outputFolder
     global recoverComments
@@ -368,31 +209,9 @@ def converter(input, output, recover_comments, archive_context, watch_folder, wa
     outputFolder = os.path.join(output, '')
     recoverComments = (recover_comments)
     context = (archive_context)
-    delete_input = (delete_input)
+    delete_input = delete_input
 
-    logging.debug("Recover Comments: " + str(recoverComments))
-    logging.debug("Recover Context: " + str(context))
-
-    #Simple watch function
-    if watch_folder:
-        oldContent = []
-        logging.info("Watching...")
-        while True:
-            content = []
-            for dirpath, dnames, fnames in os.walk(inputFolder):
-                for f in fnames:
-                    content.append(f)
-                for d in dnames:
-                    content.append(d)
-            if content != oldContent:
-                logging.info("Content found!")
-                main()
-            else:
-                logging.info("Nothing new, sleeping for " + str(watch_freq) + " minutes.")
-                time.sleep(watch_freq * 60)
-            oldContent = content
-    else:
-        main()
+    main()
     
     if delete_input:
         for root, dirs, files in os.walk(inputFolder):
