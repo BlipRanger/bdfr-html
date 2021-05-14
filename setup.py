bb7d208da490795d134f57cad57689023affbaf7
BlipRanger <1860540+BlipRanger@users.noreply.github.com>
2021-05-14 12:44:58 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'bdfrtohtml',
+  packages = ['bdfrtohtml'], # this must be the same as the name above
+  version = '1.3.0',
+  description = 'Convert the output of BDFR to HTML',
+  author = 'BlipRanger',
+  author_email = 'blipranger@shrubbery.co',
+  url = 'https://github.com/BlipRanger/bdfr-html',
+  download_url = 'https://github.com/BlipRanger/bdfr-html/releases/tag/v1.3.0',
+  keywords = ['bdfr', 'reddit', 'downloader'], 
+  classifiers = [],
+)
