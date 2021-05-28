9ed5826599ca480d3387619b73b43a7e3bf74f18
BlipRanger <1860540+BlipRanger@users.noreply.github.com>
2021-05-27 23:26:03 -0400

    Multi-user support!

--- start.py
+++ start.py
@@ -3,7 +3,11 @@ import os
 import subprocess
 import time
 import yaml
+import shutil
+import logging
 
+LOGLEVEL = os.environ.get('BDFRH_LOGLEVEL', 'INFO').upper()
+logging.basicConfig(level=LOGLEVEL)
 
 # Load in a yaml config file
 def load_config(config_file):
@@ -11,19 +15,49 @@ def load_config(config_file):
         cfg = yaml.safe_load(stream)
     return cfg
 
+def create_or_copy_config(config_filepath):
+    if not os.path.exists(config_filepath):
+        shutil.copyfile("config/default_bdfr_config.cfg", config_filepath)
+
 config_path = "config/config.yml"
 config = load_config(config_path)
 bdfr_cfg = config['bdfr']
 bdfrhtml_cfg = config['bdfrhtml']
 
+if bdfr_cfg.get('users') is not None:
+    while True:
+        for user in bdfr_cfg.get('users'):
+            bdfr_config_file = os.path.join("config/user_configs/", (user + '.cfg'))
+            create_or_copy_config(bdfr_config_file)
 
-idList = os.path.join(bdfrhtml_cfg['output_folder'], "idList.txt")
+            input_folder = os.path.join(bdfrhtml_cfg['input_folder'], (user + '/'))
+            output_folder = os.path.join(bdfrhtml_cfg['output_folder'], (user + '/'))
+            idList = os.path.join(output_folder, "idList.txt")
 
-while True:
-    if bdfr_cfg['run_bdfr']:
-        subprocess.call(["python", "-m", "bdfr", "archive", "--user", "me", "--saved", "-L", str(bdfr_cfg['limit']),
-                         "--authenticate", bdfrhtml_cfg['input_folder']])
-        subprocess.call(["python", "-m", "bdfr", "download", "--user", "me", "--saved", "-L", str(bdfr_cfg['limit']),
-                         "--exclude-id-file", idList, "--authenticate", "--file-scheme", "{POSTID}", bdfrhtml_cfg['input_folder']])
-    subprocess.call(["python", "-m", "bdfrtohtml", "--config", config_path])
-    time.sleep(int(bdfr_cfg['frequency'])*60)
+            if bdfr_cfg['run_bdfr']:
+                logging.info(f"Now running BDFR for {user}")
+                subprocess.call(["python", "-m", "bdfr", "archive", "--user", "me", "--saved", "-L", str(bdfr_cfg['limit']),
+                                "--authenticate", input_folder, "--config", bdfr_config_file])
+                subprocess.call(["python", "-m", "bdfr", "download", "--user", "me", "--saved", "-L", str(bdfr_cfg['limit']),
+                                "--exclude-id-file", idList, "--authenticate", "--file-scheme", "{POSTID}", input_folder, "--config", bdfr_config_file])
+            subprocess.call(["python", "-m", "bdfrtohtml", "--config", config_path, "--input_folder", input_folder, "--output_folder", output_folder])
+        logging.info(f"Runs complete, now waiting for {int(bdfr_cfg['frequency'])*60} minutes before next run.")
+        time.sleep(int(bdfr_cfg['frequency']))
+        
+else:
+    idList = os.path.join(bdfrhtml_cfg['output_folder'], "idList.txt")
+    input_folder = bdfrhtml_cfg['input_folder']
+    output_folder =  bdfrhtml_cfg['output_folder']
+    default_config = "config/user_configs/default_config.cfg"
+    create_or_copy_config(default_config)
+
+    while True:
+        if bdfr_cfg['run_bdfr']:
+            logging.info(f"Now running BDFR for saved posts.")
+            subprocess.call(["python", "-m", "bdfr", "archive", "--user", "me", "--saved", "-L", str(bdfr_cfg['limit']),
+                            "--authenticate", input_folder, "--config", default_config])
+            subprocess.call(["python", "-m", "bdfr", "download", "--user", "me", "--saved", "-L", str(bdfr_cfg['limit']),
+                            "--exclude-id-file", idList, "--authenticate", "--file-scheme", "{POSTID}", input_folder, "--config", default_config])
+        subprocess.call(["python", "-m", "bdfrtohtml", "--config", config_path])
+        logging.info(f"Runs complete, now waiting for {int(bdfr_cfg['frequency'])} minutes before next run.")
+        time.sleep(int(bdfr_cfg['frequency'])*60)
